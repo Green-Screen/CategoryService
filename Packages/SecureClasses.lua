525304b706697c9eab5324ec2c3f4cc9275b53b2
Green-Screen <cagurchiek18@gmail.com>
2026-09-02 21:08:08 -0400

    Final

---
+++ Packages/SecureClasses.lua
@@ -0,0 +1,4 @@
+local REQUIRED_MODULE = require(script.Parent._Index["green-screen_secureclasses@2.3.0"]["secureclasses"])
+export type MetaMethods<T> = REQUIRED_MODULE.MetaMethods<T>
+export type WrappedMetatable<T> = REQUIRED_MODULE.WrappedMetatable<T>
+return REQUIRED_MODULE
